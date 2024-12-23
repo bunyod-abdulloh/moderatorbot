4ab9333be760367f84dcea5600574f4d1b01bd14
bunyod-abdulloh <90672691+Bunyod-Abdulloh@users.noreply.github.com>
2024-12-23 20:50:14 +0500

    Commited

--- handlers/private/start.py
+++ handlers/private/start.py
@@ -2,10 +2,11 @@ from aiogram import types
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.builtin import CommandStart
 
-from loader import dp
+from loader import dp, db
 
 
 @dp.message_handler(CommandStart(), state="*")
 async def bot_start(message: types.Message, state: FSMContext):
     await message.answer(f"Salom, {message.from_user.full_name}!")
+    await db.add_user(message.from_user.id)
     await state.finish()
