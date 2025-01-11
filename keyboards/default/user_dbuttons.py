23ac025e2039592019cea1853c1d8ea6c446fb22
bunyod-abdulloh <90672691+Bunyod-Abdulloh@users.noreply.github.com>
2025-01-11 22:37:30 +0500

    Commited

--- keyboards/default/user_dbuttons.py
+++ keyboards/default/user_dbuttons.py
@@ -2,4 +2,4 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 user_main_dbuttons = ReplyKeyboardMarkup(resize_keyboard=True)
 user_main_dbuttons.row("➕ Guruhda odam ko'paytirish")
-user_main_dbuttons.row("📱 Admin bilan aloqa", "⚙️ Sozlamalar")
+user_main_dbuttons.row("📱 Admin bilan aloqa")
