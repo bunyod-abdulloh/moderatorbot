f9b798786168e0d23761ef98aa56e48647d82744
bunyod-abdulloh <90672691+Bunyod-Abdulloh@users.noreply.github.com>
2025-01-11 22:22:09 +0500

    Commited

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from aiogram import executor
 
-from data.config import ADMINS
 from loader import dp, db
 import middlewares, filters, handlers
 from utils.notify_admins import on_startup_notify
@@ -11,15 +10,15 @@ async def on_startup(dispatcher):
     await set_default_commands(dispatcher)
     await on_startup_notify(dispatcher)
     await db.create()
-    await db.drop_table_groups()
+    # await db.drop_table_groups()
     # await db.drop_table_users()
     # await db.drop_table_count_users()
     await db.create_tables()
-    for n in range(30):
-        await db.add_group(
-            telegram_id=int(ADMINS[0]),
-            group_id=-1001616939715
-        )
+    # for n in range(30):
+    #     await db.add_group(
+    #         telegram_id=int(ADMINS[0]),
+    #         group_id=-1001616939715
+    #     )
 
 
 if __name__ == '__main__':
